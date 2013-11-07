60708089855dd85a1fb6257f2754f5f5dde6b691
unknown <Owner@.(none)>
2013-11-07 16:19:16 -0500

    Added highlighting on click

--- Final/Views/Users/list.php
+++ Final/Views/Users/list.php
@@ -1,7 +1,7 @@
 <link href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/css/jquery.dataTables.min.css" type="text/css" rel="stylesheet" />
 <style>
-	tr.success, tr.success td{
-		background-color: #00AA00;
+	tr.success2, tr.success2 td{
+		background-color: #FFAA00 !important;
 	}
 </style>
 
@@ -53,6 +53,10 @@
         $(".table").dataTable();
         $(".alert .close").click(function(){
         	$(this).closest(".alert").slideUp();
+        });
+        
+        $(".table tr").click(function(){
+        	$(this).toggleClass("success2");
         });
     })
     </script>
