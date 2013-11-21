b8c1abdd8b920c107c973e16412c8605061f2305
unknown <Owner@.(none)>
2013-11-21 16:19:43 -0500

    Show/HideDialog

--- Final/Views/Users/list.php
+++ Final/Views/Users/list.php
@@ -1,4 +1,6 @@
 <link href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/css/jquery.dataTables.min.css" type="text/css" rel="stylesheet" />
+<link href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" type="text/css" rel="stylesheet" />
+
 <style>
 	tr.success2, tr.success2 td{
 		background-color: #FFAA00 !important;
@@ -21,24 +23,24 @@
 		</div>
 	<? endif ?>
 	
-	<a href="?action=new">Add Contact</a>
+	<a href="?action=new" id="add-link">Add Contact</a>
 	
 	<div id="table-wrapper" class="col-md-12">
-	<table class="table table-hover table-bordered table-striped">
-		<thead>
-		<tr>
-			<th>First Name</th>
-			<th>Last Name</th>
-			<th>Type</th>
-			<th></th>
-		</tr>
-		</thead>
-		<tbody>
-		<? foreach ($model as $rs): ?>
-			<? include 'item.php'; ?>
-		<? endforeach ?>
-		</tbody>
-	</table>
+		<table class="table table-hover table-bordered table-striped">
+			<thead>
+			<tr>
+				<th>First Name</th>
+				<th>Last Name</th>
+				<th>Type</th>
+				<th></th>
+			</tr>
+			</thead>
+			<tbody>
+			<? foreach ($model as $rs): ?>
+				<? include 'item.php'; ?>
+			<? endforeach ?>
+			</tbody>
+		</table>
 	</div>
 	<div id="details" class="col-md-6"></div>
 	<div id="myModal" class="modal slide"></div>
@@ -56,53 +58,43 @@
 </script>
 	
 <script id="tbody-template" type="text/x-handlebars-template">
-	{{#each}}
-		<tr>
-			<td>{{FirstName}}</td> 
-			<td>{{LastName}}</td>
-			<td>{{UserType_Name}}</td>
-			<td>
-				<a class="glyphicon glyphicon-file" href="?action=details&id={{id}}"></a>
-				<a class="glyphicon glyphicon-pencil" href="?action=edit&id={{id}}"></a>
-				<a class="glyphicon glyphicon-trash" href="?action=delete&id={{id}}"></a>
-			</td>
-		</tr>
-	{{/each}}
+	{{#each .}}
+        <tr>
+            {{> row-template}}
+        </tr>
+    {{/each}}
 </script>
 
 <? function Scripts(){ ?>
+	<? global $model; ?>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.1.2/handlebars.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 	<script type="text/javascript">
 	$(function(){
-		var tableTemplate = Handlebars.compile($("#tbody-template").html());
-		$(".table tbody").html(tableTemplate(<?=json_encode($model)?>));
+		var templateRow = Handlebars.compile($("#row-template").html());
+        Handlebars.registerPartial("row-template", templateRow);                                
+        var tableTemplate = Handlebars.compile($("#tbody-template").html());
+                                                
+        $(".table tbody").html(tableTemplate(<?=json_encode($model);?>))    
 
         $(".table").dataTable();
+        
         $(".alert .close").click(function(){
         	$(this).closest(".alert").slideUp();
         });
-        
-        /*
-        $(".table tr").click(function(){
-        	$(this).toggleClass("success2");
-        });
-        */
        
+       	$("#add-link").click(function(){
+       		ShowDialog(this.href);
+       		return false;
+       	});
+       	
         $(".table a").click(function(){
             if($(this).closest("tr").hasClass("success2")){
-                $(".success2").removeClass("success2");
-                $("#table-wrapper").removeClass("col-md-6").addClass("col-md-12");
-                $("#details").html('');
+                HideDialog();
             }else{
-                $(".success2").removeClass("success2");
-                $(this).closest("tr").addClass("success2");
-                $("#table-wrapper").removeClass("col-md-12").addClass("col-md-6");
-                
-                $("#details").load(this.href, {format: "plain"}, function(){
-                	$("#details form").submit(HandleSubmit);
-                });
+                ShowDialog(this.href, $(this).closest("tr"));
             }
             
             return false;
@@ -110,17 +102,40 @@
         
         var HandleSubmit = function (){
         	var data = $(this).serializeArray();
-        	data.push({name:'format', value:'json'});
+            data.push({name:'format', value:'json'});
             $.post(this.action, data, function(results){
-            	if(!results.errors){
-            		$("#details").html(results);
-            	}else{
-            		var template = Handlebars.compile($("#row-template").html());
-            		$(".success2").html(template(results.model));
-            	}
+	            if(results.errors){
+                    for(k in results.errors){
+                    	toastr.error(k + ' ' + results.errors[k], "Could not save");
+                    	var e = $('#' + k)
+                    		.after($('<span class="control-label" />').html(results.errors[k]))
+                    		.closest('.form-group').addClass('has-error');
+                    }                                        
+	            }else{
+                    $(".success2").html(templateRow(results.model));
+                    toastr.success("Your record has been saved!", "Success");
+	            }
+                    
             }, 'json');
             
             return false;
+        }
+        
+        var ShowDialog = function(url, selectedRow){
+        	$(".success2").removeClass("success2");
+        	if(selectedRow){
+        		selectedRow.addClass("success2");
+        	}
+            $("#table-wrapper").removeClass("col-md-12").addClass("col-md-6");
+            $("#details").load(url, {format: "plain"}, function(){
+            	$("#details form").submit(HandleSubmit);
+            });
+        }
+        
+        var HideDialog = function(){
+        	$(".success2").removeClass("success2");
+            $("#table-wrapper").removeClass("col-md-6").addClass("col-md-12");
+            $("#details").html('');
         }
     })
     </script>
