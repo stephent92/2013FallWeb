976581be6480aee27af681ad4a18a7841482caf9
unknown <Owner@.(none)>
2013-11-22 12:51:25 -0500

    Added pager button

--- Final/Views/FrontEnd/list.php
+++ Final/Views/FrontEnd/list.php
@@ -4,9 +4,18 @@
 	
 	<? foreach ($model as $rs): ?>
 		<div class="well">
-			<img src="<?=$rs['Img']?>" href="?action=details&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="#myModal" width="128" class="btn btn-default"></img>
-			<?=$rs['Item']?>
-			$<?=$rs['Price']?>
+			<div class="media">
+				<a class="pull-left" href="#">
+					<img src="<?=$rs['Img']?>" href="?action=details&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="#myModal" width="128" class="btn btn-default"></img>
+				</a>
+				<div class="media-body">
+					<h4 class="media-heading"><?=$rs['Item']?></h4>
+					<?=$rs['Description']?><br>
+				</div>
+				<ul class="pager">
+					<li class="previous"><a href="?action=details&id=<?=$rs['id']?>&format=dialog" data-toggle="modal" data-target="#myModal">$<?=$rs['Price']?></a></li>
+				</ul>
+			</div>
 		</div>
 	<? endforeach ?>
 </div>
